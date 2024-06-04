2e73d3c9e94a8d197ccf2662b9ba460e32c70f12
Joshua Jacob <josh11jac16@gmail.com>
2024-06-03 21:30:11 -0700

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require("@apollo/server");
-const { typeDefs, resolvers } = require("./schemas");
+const { typeDefs, resolvers } = require("./schema");
 const { authMiddleware } = require("./utils/auth");
 const path = require('path');
 const db = require('./config/connection');
