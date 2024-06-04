ec34405ffd40430a4ebaa911a57e4b55436ff9a3
Joshua Jacob <josh11jac16@gmail.com>
2024-06-03 21:17:46 -0700

    edited server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer } = require("@apollo/server");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
 const path = require('path');
